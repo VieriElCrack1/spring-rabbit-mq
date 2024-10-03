58e71a1697162290208d769cbad413ad81866b74
VieriElCrack1 <santosvieri9@gmail.com>
2024-10-03 11:38:09 -0500

    iniciando proyecto

---
+++ src/test/java/pe/edu/cibertec/spring_rabbit_mq/SpringRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.spring_rabbit_mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
